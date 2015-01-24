fabb1919928dd313a6a9442aa7e57b741b434b57
withoutairs <github@ktcb.com>
2015-01-24 15:30:40 -0600

    ugly backwards lookup

--- munge.rb
+++ munge.rb
@@ -7,22 +7,55 @@ CSV::Converters[:mint] = lambda { |s|
     s
   end
 }
-csv = CSV.read('small.csv', :headers => true, :converters => [:all, :mint])
+csv = CSV.read('2015.csv', :headers => true, :converters => [:all, :mint])
 categories_seen = {}
+categories_wanted = [
+'Home Improvement',
+'1508 Mortgage',
+'2238 Mortgage',
+'Shopping',
+'Clothes',
+'Groceries',
+'Restaurants',
+'Kids Activities',
+'Auto Payment',
+'Education',
+'Vacation',
+'Gym',
+'Business Expense',
+'Utilities',
+'Car',
+'Property Tax',
+'Financial',
+'Mobile Phone',
+'Charity',
+'Internet',
+'Personal Care',
+'Cash',
+'Auto Service',
+'Pharmacy',
+]
 csv.each { |row|
-  date = row["Date"]
-  row << ["Month", date.strftime("%m")]
-  row << ["Year", date.strftime("%Y")]
-  row << ["Sortable", date.strftime("%Y-%m")]
-  category = row["Category"]
-  amount = row["Amount"]
+  date = row['Date']
+  row << ['Month', date.strftime('%m')]
+  row << ['Year', date.strftime('%Y')]
+  row << ['Sortable', date.strftime('%Y-%m')]
+
+  next unless date.strftime('%Y') == '2015'
+
+  category = row['Category']
+
+  row['Category'] = 'Car' if category == 'Parking'
+  row['Category'] = 'Car' if category == 'Rental Car & Taxi'
+
+  category = row['Category']
+  amount = row['Amount']
   tally = categories_seen[category] ? categories_seen[category] : {:count => 0, :amount => 0}
   tally[:count] += 1
   tally[:amount] += amount
   categories_seen[category] = tally
-
-  row["Category"] = "XXX" if (category == "Parking")
+  puts "#{category} was not expected: $#{amount} #{row['Description']} on #{date}" unless (categories_wanted.include?(category))
 }
-pp categories_seen.sort_by {|category, tally| -tally[:count]}
+pp categories_seen.sort_by {|_, tally| -tally[:count]}
 
 
